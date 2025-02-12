d328edc4d6b44cf43f5efa2185fece65c1cf58e0
F1End <horvirich@gmail.com>
2025-02-12 14:18:45 +0100

    Initual work on check module including tests: completed some loader functions

--- src/check.py
+++ src/check.py
@@ -1,13 +1,73 @@
 """
-Run checks on file output (website content) fetch module
+Run checks on file output (website content) from fetch module
 """
+from io import BytesIO
+import requests
+
+import html5lib
+from bs4 import BeautifulSoup
 
 # class to load file to run checks on
+class FileCheck:
+    def __init__(self, checked_file: str, check_type: str, check_against: str, tolerance: float = 0.95):
+        self.checked_file = checked_file
+        self.check_type = check_type
+        self.check_agaist = check_against
+        self.tolerance = tolerance
+        self.check_file_content = None
+        self.check_against_content = None
 
-# method to get file size
+    def run_check(self):
+        raise NotImplemented
 
-# method to compare file sizes
+    def check_html(self) -> str:
+        htmlparser = html5lib.HTMLParser(strict=True)
+        try:
+            htmlparser.parse(BytesIO(self.check_against_content))
+            return "HTML is valid"
+        except Exception as e:
+            return f"Errors occured during loading: {e}"
+        # try:
+        #     soup = BeautifulSoup(self.check_file_content, 'html.parser')
+        #     if soup.find():
+        #         return "HTML is valid"
+        # except Exception as e:
+        #     return f"Errors occured during loading: {e}"
 
-# method to get url thing
 
-# method to check html structure
+
+    def check_size(self):
+        raise NotImplemented
+
+    def check_content(self):
+        raise NotImplemented
+
+    def check_minimum_content(self):
+        raise NotImplemented
+
+    def _load_contents(self, against_file: bool = True):
+        self.check_file_content = self._load_from_file(self.checked_file)
+        if against_file:
+            self.check_against_content = self._load_from_file(self.check_agaist)
+            print(f"Checking against {self.check_agaist}")
+        else:
+            self.check_against_content = self._load_from_url(self.check_agaist)
+
+    def _load_from_file(self, file_path) -> str:
+        with open(file_path,"r") as file:
+            data = file.read()
+        return data
+
+    def _load_from_url(self, url: str) -> str:
+        response = requests.get(url)
+        content = response.content
+        return content
+
+    def _get_size(self, content: str) -> int:
+        raise NotImplemented
+
+    def _get_file_content(self):
+        raise NotImplemented
+
+    def _load_url_content(self):
+        raise NotImplemented
--- tests/test_check.py
+++ tests/test_check.py
@@ -1,10 +1,125 @@
-import unittest
+from unittest import TestCase, main
+from unittest.mock import MagicMock, patch, call
+
+from src import check
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+class TestFileCheck(TestCase):
+
+    def setUp(self):
+        file = "webpage.html"
+        check_type = "size"
+        old_file = "webpage_old.html"
+        self.test_filecheck = check.FileCheck(file, check_type, old_file)
+
+    def test_init(self):
+        file = "webpage.html"
+        check_type = "size"
+        old_file = "webpage_old.html"
+        tol = 0.97
+        test_filecheck = check.FileCheck(file, check_type, old_file, tol)
+        self.assertEqual(test_filecheck.checked_file, file)
+        self.assertEqual(test_filecheck.check_type, check_type)
+        self.assertEqual(test_filecheck.check_agaist, old_file)
+        self.assertEqual(test_filecheck.tolerance, tol)
+
+    def test_get_file_size(self):
+        pass
+
+    def test_check_html(self):
+        pass
+        # file = "webpage.html"
+        # check_type = "size"
+        # old_file = "webpage_old.html"
+        # tol = 0.97
+        # test_filecheck = check.FileCheck(file, check_type, old_file, tol)
+        #
+        # # Case 1: valid html
+        # valid_html = """<!DOCTYPE html><html><head></head><body></body></html>"""
+        # test_filecheck.check_file_content = valid_html
+        # expected = "HTML is valid"
+        # validate = test_filecheck.check_html()
+        # self.assertEqual(validate, expected)
+        #
+        # # Case2: invalid html
+        # invalid_html = """<!DOCTYPE html><html><head></head>"""
+        # # invalid_html = """<html><head></head></html>"""
+        # test_filecheck.check_file_content = invalid_html
+        # expected = True
+        # validate = test_filecheck.check_html()
+        # print(validate)
+        # # self.assertEqual(validate, expected)
+
+    def test_check_size(self):
+        pass
+
+    @patch('src.check.FileCheck._load_from_file')
+    @patch('src.check.FileCheck._load_from_url')
+    def test__load_contents(self, load_url_mock, load_file_mock):
+        # Case 1: Load file + load file
+        load_file_return_values = ["Content_for_first_call", "Content_for_second_call",
+                                   "Content_for_third_call"]
+        load_url_return_value = "Content_from_url"
+        load_file_mock.side_effect = load_file_return_values
+        load_url_mock.return_value = load_url_return_value
+        path_file = "some/path/file.html"
+        path_old_file = "some/path/old_file.html"
+        expected_file_calls = [call(path_file), call(path_old_file)]
+
+        self.test_filecheck.checked_file = path_file
+        self.test_filecheck.check_agaist = path_old_file
+        self.test_filecheck._load_contents()
+        load_file_mock.assert_has_calls(expected_file_calls)
+        load_url_mock.assert_not_called()
+        self.assertEqual(self.test_filecheck.check_file_content, load_file_return_values[0])
+        self.assertEqual(self.test_filecheck.check_against_content, load_file_return_values[1])
+
+        load_file_mock.reset_mock()
+
+        # Case 2: Load file + load url
+        load_file_return_value = ["Content_for_single_call"]
+        load_file_mock.side_effect = load_file_return_value
+        url = "somewebsite.com"
+
+        self.test_filecheck.check_agaist = url
+        self.test_filecheck._load_contents(against_file=False)
+        load_file_mock.assert_called_once()
+        load_file_mock.assert_called_with(path_file)
+        load_url_mock.assert_called_once()
+        load_url_mock.assert_called_with(url)
+        self.assertEqual(self.test_filecheck.check_file_content, load_file_return_value[0])
+        self.assertEqual(self.test_filecheck.check_against_content, load_url_return_value)
+
+
+    @patch('src.check.open')
+    def test__load_from_file(self, open_mock):
+        file_mock = MagicMock()
+        fake_content = "<some html file content>"
+        file_mock.read.return_value = fake_content
+        open_mock.return_value.__enter__.return_value = file_mock
+        file_path = "path/to/some/file"
+
+        output = self.test_filecheck._load_from_file(file_path)
+        open_mock.assert_called_with(file_path, "r")
+        file_mock.read.assert_called_once()
+
+        self.assertEqual(output, fake_content)
+
+    @patch('src.check.requests')
+    def test__load_from_url(self, requests_mock):
+        request_get_mock = MagicMock()
+        fake_site_content = "<Some content downloaded from web>"
+        request_get_mock.content = fake_site_content
+        requests_mock.get.return_value = request_get_mock
+        check_against_url = "some_web_url"
+        self.test_filecheck.check_agaist = check_against_url
+
+        output = self.test_filecheck._load_from_url(check_against_url)
+        requests_mock.get.assert_called_with(check_against_url)
+        self.assertEqual(output, fake_site_content)
+
+
 
 
 if __name__ == '__main__':
-    unittest.main()
+    main()
