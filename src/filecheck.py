815005705fbe2e27cbe079ae8ee3194fd520da31
F1End <horvirich@gmail.com>
2025-02-17 15:29:01 +0100

    Some refactoring on the filechecker module; includes some new methods and other clean-up; tests updated accordingly

--- src/filecheck.py
+++ src/filecheck.py
@@ -2,31 +2,52 @@
 Run checks on file output (website content) from fetch module
 """
 import requests
+from typing import Optional
+from pathlib import Path
 
 from src import validatehtml
 
 
 class FileCheck:
-    def __init__(self, checked_file: str, check_type: str, check_against: str, tolerance: float = 0.05):
+    def __init__(self, checked_file: str, check_type: dict, tolerance: float = 0.05):
         self.checked_file = checked_file
         self.check_type = check_type
-        self.check_against = check_against
         self.tolerance = tolerance
         self.check_file_content = None
         self.check_against_content = None
         self.check_map = {"html_structure": self.check_html,
-                          "size_change": self.check_size}
+                          "against_site": self.check_against_site,
+                          "against_file": self.check_against_file}
 
-    def run_check(self):
+    def run_checks(self):
         issues = []
-        self._load_contents()
-        for check in self.check_type:
-            flagged = self.check_map[check]()
-            issues.append(flagged)
-        return issues
+        for check, param in self.check_type.items():
+            flagged = self.check_map[check](param)
+            if flagged:
+                issues.append(flagged)
+        if issues:
+            raise Exception(issues)
 
-    def check_html(self) -> str:
-        errors = validatehtml.ChkHtmlStructure().run_check(self.check_file_content)
+    def check_against_file(self, check_against: str) -> Optional[str]:
+        self._load_contents(check_against, against_file=True)
+        ratio = self.check_size()
+        if ratio:
+            return self._format_size_diff_msg(check_against, ratio)
+
+    def check_against_site(self, check_against: str) -> Optional[str]:
+        self._load_contents(check_against, against_file=False)
+        ratio = self.check_size()
+        if ratio:
+            return self._format_size_diff_msg(check_against, ratio)
+
+    def _format_size_diff_msg(self, check_against: str, ratio: float):
+        msg = f"Size difference is {ratio} against {check_against}" \
+              f", which is above threshold {self.tolerance}."
+        return msg
+
+    def check_html(self, full_check: bool = True) -> str:
+        self.check_file_content = self._load_from_file(self.checked_file)
+        errors = validatehtml.ChkHtmlStructure(full_check=full_check).run_checks(self.check_file_content)
         return errors
 
     def check_size(self):
@@ -42,15 +63,16 @@ class FileCheck:
     def check_minimum_content(self):
         raise NotImplemented
 
-    def _load_contents(self, against_file: bool = True):
+    def _load_contents(self, check_against: str, against_file: bool = True):
         self.check_file_content = self._load_from_file(self.checked_file)
         if against_file:
-            self.check_against_content = self._load_from_file(self.check_agaist)
+            self.check_against_content = self._load_from_file(check_against)
         else:
-            self.check_against_content = self._load_from_url(self.check_agaist)
+            self.check_against_content = self._load_from_url(check_against)
 
     def _load_from_file(self, file_path) -> str:
-        with open(file_path, "r") as file:
+        file_path = Path(file_path)
+        with open(file_path) as file:
             data = file.read()
         return data
 
--- tests/test_filecheck.py
+++ tests/test_filecheck.py
@@ -14,16 +14,54 @@ class TestFileCheck(TestCase):
 
     def test_init(self):
         file = "webpage.html"
-        check_type = "size"
-        old_file = "webpage_old.html"
+        check_type = {"html_structure": True}
         tol = 0.07
-        test_filecheck =filecheck.FileCheck(file, check_type, old_file, tol)
+        test_filecheck =filecheck.FileCheck(file, check_type, tol)
         self.assertEqual(test_filecheck.checked_file, file)
         self.assertEqual(test_filecheck.check_type, check_type)
-        self.assertEqual(test_filecheck.check_against, old_file)
         self.assertEqual(test_filecheck.tolerance, tol)
 
-    @patch('src.filecheck._get_size')
+    @patch('src.filecheck.FileCheck.check_html')
+    @patch('src.filecheck.FileCheck.check_against_site')
+    @patch('src.filecheck.FileCheck.check_against_file')
+    def test_run_checks(self, mock_html_chk, mock_site_chk, mock_file_chk):
+        html_value = True
+        url = "somewebsite.com/page.html"
+        old_file = "path/to/my/file.html"
+        # need to mack check_map due to patch not being able to mock calls this way
+        fake_check_map = {"html_structure": mock_html_chk,
+                          "against_site": mock_site_chk,
+                          "against_file": mock_file_chk}
+        mock_file_chk.return_value = None
+        mock_site_chk.return_value = None
+        mock_html_chk.return_value = None
+
+        # Case 1: No issues are found
+        checks = {"html_structure": True,
+                  "against_site": url,
+                  "against_file": old_file}
+        self.test_filecheck.check_map = fake_check_map
+        self.test_filecheck.check_type = checks
+        self.test_filecheck.run_checks()
+        mock_file_chk.assert_called_with(old_file)
+        mock_site_chk.assert_called_with(url)
+        mock_html_chk.assert_called_with(html_value)
+
+        mock_file_chk.reset_mock()
+        mock_site_chk.reset_mock()
+        mock_html_chk.reset_mock()
+
+        # Case 2: At least some issue is found
+        mock_file_chk.return_value = None
+        mock_site_chk.return_value = None
+        mock_html_chk.return_value = "Some html issue is returned"
+
+        self.assertRaises(Exception,self.test_filecheck.run_checks)
+        mock_file_chk.assert_called_with(old_file)
+        mock_site_chk.assert_called_with(url)
+        mock_html_chk.assert_called_with(html_value)
+
+    @patch('src.filecheck.FileCheck._get_size')
     def test_check_size(self, get_size_mock):
         tolerance = 0.05
         check_content = "Some str of 100 characters"
@@ -33,7 +71,7 @@ class TestFileCheck(TestCase):
         self.test_filecheck.check_against_content = check_against_content
 
         # Case 1: Within tolerance
-        get_size_mock.side_effect = [100, 104]
+        get_size_mock.side_effect = [100, 104, 100]
         expected_calls = [call(check_content), call(check_against_content)]
         expected_return = None
         result = self.test_filecheck.check_size()
@@ -41,7 +79,7 @@ class TestFileCheck(TestCase):
         self.assertEqual(result, expected_return)
 
         # Case 2: Larger than tolerance
-        get_size_mock.side_effect = [100, 106]
+        get_size_mock.side_effect = [100, 106, 100]
         expected_calls = [call(check_content), call(check_against_content)]
         expected_return_2 = 6/100
         result = self.test_filecheck.check_size()
@@ -58,21 +96,86 @@ class TestFileCheck(TestCase):
         self.assertEqual(size, s)
 
     @patch('src.validatehtml.ChkHtmlStructure')
-    def test_check_html(self, chkhtml_mock):
+    @patch('src.filecheck.FileCheck._load_from_file')
+    def test_check_html(self, load_from_f_mock, chkhtml_mock):
         instance_mock = MagicMock()
         chkhtml_mock.return_value = instance_mock
         errors = {'unclosed_opening': ['html'], 'unexpected_closing': ['/body']}
-        instance_mock.run_check.return_value = errors
+        instance_mock.run_checks.return_value = errors
         content = "some-html-content"
-        self.test_filecheck.check_file_content = content
+        load_from_f_mock.return_value = content
 
         html_errors = self.test_filecheck.check_html()
         self.assertEqual(html_errors, errors)
-        chkhtml_mock.assert_called_once()
-        instance_mock.run_check.assert_called_with(content)
+        chkhtml_mock.assert_called_with(full_check=True)
+        load_from_f_mock.assert_called_with(self.test_filecheck.checked_file)
+        instance_mock.run_checks.assert_called_with(content)
 
-    def test_check_size(self):
-        pass
+    @patch('src.filecheck.FileCheck._load_contents')
+    @patch('src.filecheck.FileCheck.check_size')
+    @patch('src.filecheck.FileCheck._format_size_diff_msg')
+    def test_check_against_file(self, mock_format_msg, mock_check_size, mock_load_cont):
+        # Case 1: Ratio is ok (no return)
+        mock_check_size.return_value = None
+        old_file_str = "some/file/path.html"
+
+        ratio = self.test_filecheck.check_against_file(old_file_str)
+        mock_load_cont.assert_called_with(old_file_str, against_file=True)
+        mock_check_size.assert_called_once()
+        mock_format_msg.assert_not_called()
+        self.assertEqual(ratio, None)
+
+        mock_check_size.reset_mock()
+
+        # Case 2: Ratio is not ok -> return value
+        diff = 0.15
+        mock_check_size.return_value = diff
+        old_file_str = "some/file/path.html"
+        return_msg = "Formatted_msg"
+        mock_format_msg.return_value = return_msg
+
+        ratio = self.test_filecheck.check_against_file(old_file_str)
+        mock_load_cont.assert_called_with(old_file_str, against_file=True)
+        mock_check_size.assert_called_once()
+        mock_format_msg.assert_called_with(old_file_str, diff)
+        self.assertEqual(ratio, return_msg)
+
+    @patch('src.filecheck.FileCheck._load_contents')
+    @patch('src.filecheck.FileCheck.check_size')
+    @patch('src.filecheck.FileCheck._format_size_diff_msg')
+    def test_check_against_site(self, mock_format_msg, mock_check_size, mock_load_cont):
+        # Case 1: Ratio is ok (no return)
+        mock_check_size.return_value = None
+        url = "somewebsite.com/page.html"
+
+        ratio = self.test_filecheck.check_against_site(url)
+        mock_load_cont.assert_called_with(url, against_file=False)
+        mock_check_size.assert_called_once()
+        mock_format_msg.assert_not_called()
+        self.assertEqual(ratio, None)
+
+        mock_check_size.reset_mock()
+
+        # Case 2: Ratio is not ok -> return value
+        diff = 0.15
+        mock_check_size.return_value = diff
+        old_file_str = "some/file/path.html"
+        return_msg = "Formatted_msg"
+        mock_format_msg.return_value = return_msg
+
+        ratio = self.test_filecheck.check_against_site(url)
+        mock_load_cont.assert_called_with(url, against_file=False)
+        mock_check_size.assert_called_once()
+        mock_format_msg.assert_called_with(url, diff)
+        self.assertEqual(ratio, return_msg)
+
+    def _format_size_diff_msg(self):
+        ratio = 0.1
+        check_against = "path/to/old_file.html"
+        expected_msg = f"Size difference is {ratio} against {check_against}" \
+                       f", which is above threshold {self.test_filecheck.tolerance}."
+        actual_msg = self.test_filecheck._format_size_diff_msg(check_against, ratio)
+        self.assertEqual(expected_msg, actual_msg)
 
     @patch('src.filecheck.FileCheck._load_from_file')
     @patch('src.filecheck.FileCheck._load_from_url')
@@ -88,8 +191,7 @@ class TestFileCheck(TestCase):
         expected_file_calls = [call(path_file), call(path_old_file)]
 
         self.test_filecheck.checked_file = path_file
-        self.test_filecheck.check_agaist = path_old_file
-        self.test_filecheck._load_contents()
+        self.test_filecheck._load_contents(path_old_file)
         load_file_mock.assert_has_calls(expected_file_calls)
         load_url_mock.assert_not_called()
         self.assertEqual(self.test_filecheck.check_file_content, load_file_return_values[0])
@@ -102,8 +204,7 @@ class TestFileCheck(TestCase):
         load_file_mock.side_effect = load_file_return_value
         url = "somewebsite.com"
 
-        self.test_filecheck.check_agaist = url
-        self.test_filecheck._load_contents(against_file=False)
+        self.test_filecheck._load_contents(url, against_file=False)
         load_file_mock.assert_called_once()
         load_file_mock.assert_called_with(path_file)
         load_url_mock.assert_called_once()
@@ -112,15 +213,19 @@ class TestFileCheck(TestCase):
         self.assertEqual(self.test_filecheck.check_against_content, load_url_return_value)
 
     @patch('src.filecheck.open')
-    def test__load_from_file(self, open_mock):
+    @patch('src.filecheck.Path')
+    def test__load_from_file(self, path_mock, open_mock):
         file_mock = MagicMock()
         fake_content = "<some html file content>"
         file_mock.read.return_value = fake_content
         open_mock.return_value.__enter__.return_value = file_mock
         file_path = "path/to/some/file"
+        path_output = f"Path{file_path}"
+        path_mock.return_value = path_output
 
         output = self.test_filecheck._load_from_file(file_path)
-        open_mock.assert_called_with(file_path, "r")
+        path_mock.assert_called_with(file_path)
+        open_mock.assert_called_with(path_output)
         file_mock.read.assert_called_once()
 
         self.assertEqual(output, fake_content)
